1d6ef57c73be54faa669e271a59a0cacdfb9d73f
George Oblapenko <kunstmord@kunstmord.com>
2018-07-25 17:44:36 +0300

    Removed the correlation and unimportant features computation stuff

--- src/dataprocessor.py
+++ src/dataprocessor.py
@@ -6,22 +6,21 @@ import numpy as np
 
 
 class DataProcessor:
-    def __init__(self, path: str, df, non_feature_columns=None, correlation_threshold=1.0,
-                 importance_threshold=-1.0, fname_prefix='', verbose=True):
+    def __init__(self, path: str, df, non_feature_columns=None, fname_prefix='', verbose=True):
         if not path.endswith('/'):
             path += '/'
         self.base_path = path
         self.fname = fname_prefix + str(datetime.now()).replace(':', '_').replace(' ', '_')[5:19]
+        self.removers = []
+        self.remover_params = []
         self.transforms = []
         self.transform_params = []
 
         self.features = {'all': [col for col in df.columns if col not in non_feature_columns],
                          'selected': [],
-                         'correlated': [],
-                         'unimportant': []}
+                         'removed': []
+                        }
 
-        self.correlation_threshold = correlation_threshold
-        self.importance_threshold = importance_threshold
         self.non_feature_columns = non_feature_columns  # stuff like label, filename, etc.
 
         self.verbose = verbose
@@ -37,41 +36,35 @@ class DataProcessor:
 
         pathlib.Path(path + 'dataprocessor_files/settings').mkdir(parents=True, exist_ok=True)
 
-    def find_correlated_features(self, df: pd.DataFrame, use_features: str='all', correlation_threshold=None):
-        if correlation_threshold is not None:
-            self.correlation_threshold = correlation_threshold
-
-        corr = df[self.features[use_features]].corr()
-        corr = corr.abs()
-        upper = corr.where(np.triu(np.ones(corr.shape), k=1).astype(np.bool))
-        self.features['correlated'] = [col for col in upper.columns if any(upper[col] > self.correlation_threshold)]
-
-        if self.verbose:
-            print(str(len(self.features['correlated']))
-                  + 'features found with a correlation higher than ' + str(self.correlation_threshold))
-        self.features['selected'] = [x for x in self.features['all'] if x not in self.features['correlated']]
-        return self.features['selected'], self.features['correlated']
-
     def return_features_list(self, use_features: str='selected'):
         """
-        Return list of features - return either all features, or selected features, correlated, unimportant,
+        Return list of features - return either all features, or selected features, or removed features,
         or some union of these
         :param use_features:
         :return:
         """
-        if use_features in ('selected', 'correlated', 'unimportant', 'all'):
-            return self.features[use_features]
-        elif '_' in use_features:
-            split = use_features.split('_')
-            output = [self.features[x] for x in split]
-            output = list(set(list(chain(*output))))
-            return output
+        return self.features[use_features]
+
+    def add_remover(self, remover, remover_params):
+        self.removers.append(remover(**remover_params))
+        self.remover_params.append(remover_params)
+
+    def fit_remove(self, df):
+        current_features_to_use = self.return_features_list('all')
+        features_removed = []
+        for remover, remover_params in zip(self.removers, self.remover_params):
+            current_features_to_use, to_be_removed = remover(df, current_features_to_use)
+            features_removed += to_be_removed
+        features_removed = list(set(features_removed))
+        self.features['removed'] = features_removed
+        self.features['selected'] = current_features_to_use
 
     def add_transform(self, transform, transform_params):
-        self.transforms.append(transform)
+        self.transforms.append(transform(**transform_params))
         self.transform_params.append(transform_params)
+        return self
 
-    def fit_transform(self, df,  use_features: str='selected'):
+    def fit_transform(self, df, use_features: str='selected'):
         """
         Fit and apply transforms to a dataframe
         :param df:
@@ -82,7 +75,7 @@ class DataProcessor:
         features_to_use = self.return_features_list(use_features)
 
         for transform, transform_params in zip(self.transforms, self.transform_params):
-            df[features_to_use] = transform.fit_transform(df[features_to_use], **transform_params)
+            df[features_to_use] = transform.fit_transform(df[features_to_use])
 
     def transform(self, df, use_features: str='selected'):
         """
