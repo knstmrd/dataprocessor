6c13f9791cb0cad3fa11eb303138fa404714915f
George Oblapenko <kunstmord@kunstmord.com>
2018-07-25 17:53:53 +0300

    test for removal of correlated and almost-constant features using the DataProcessor class

--- test/test_dataprocessor.py
+++ test/test_dataprocessor.py
@@ -2,16 +2,16 @@ from os.path import isfile, isdir
 from shutil import rmtree
 from os import getcwd
 import unittest
-from src import dataprocessor
+from src import dataprocessor, removers
 import pandas as pd
 
 
 class DataProcessorTest(unittest.TestCase):
     def setUp(self):
         self.curr_dir = getcwd() + '/'
-        self.df = pd.DataFrame({'x': [-2, 0, 2], 'x_p_1': [-1, 1, 3], 'x_t_x': [4, 0, 4], 'nonfeat': ['a', 'b', 'c']})
-        self.dataprocessor = dataprocessor.DataProcessor(self.curr_dir, self.df, non_feature_columns=['nonfeat'],
-                                                         correlation_threshold=0.5)
+        self.df = pd.DataFrame({'x': [-2, 0, 2], 'x_p_1': [-1, 1, 3], 'x_t_x': [4, 0, 4], 'nonfeat': ['a', 'b', 'c'],
+                                'const': [10, 10, 10]})
+        self.dataprocessor = dataprocessor.DataProcessor(self.curr_dir, self.df, non_feature_columns=['nonfeat'])
 
     def tearDown(self):
         rmtree(self.curr_dir + 'dataprocessor_files/')
@@ -30,8 +30,14 @@ class DataProcessorTest(unittest.TestCase):
         self.assertNotIn('nonfeat', self.dataprocessor.features['all'])
         self.assertIn('x', self.dataprocessor.features['all'])
 
-    def test_find_correlated_features(self):
-        selected, correlated = self.dataprocessor.find_correlated_features(self.df)
+    def test_removing_features(self):
+        self.dataprocessor.add_remover(removers.CorrelatedFeatureRemover, {'correlation_threshold': 0.5})
+        self.dataprocessor.add_remover(removers.AlmostConstantFeatureRemover, {'max_count_percent': 80})
+        selected, removed = self.dataprocessor.fit_remove(self.df)
         self.assertIn('x', selected)  # we keep the first feature
         self.assertNotIn('x_p_1', selected)  # we don't keep the second feature
-        self.assertIn('x_p_1', correlated)  # we store the second feature in a separate field
+        self.assertIn('x_p_1', removed)  # we store the second feature in a separate field
+        self.assertNotIn('nonfeat', selected)
+        self.assertNotIn('nonfeat', removed)
+        self.assertNotIn('const', selected)
+        self.assertIn('const', removed)
